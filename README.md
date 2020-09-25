4a5d60edb08aad1779bf3a0aeafe38ef855d25c0
aaloy <aaloy@apsl.net>
2020-09-25 21:35:42 +0200

    mejoramos para ir por número de vídeo

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vcs2020
 Script de conversión de vídeo para la PyconES 2020 Covid Edition
+
+Es un script guarro y rápido para la creación de los vídeos finales.
+
+NSFAQ
+
+* Solamente tengo los png de final
+
+Puedes ejecutar un  ```python3 create.py --generate all```
+
+* ¿Cómo funciona?
+
+El vídeo y las carátulas deben empezar por un mismo número, por ejemplo 01-(algo).mp4 para el vídeo y
+01-(otro algo carttula)_(resolutcion).png para la carátula. El sistema intentará elegir la mejor carátula
+según la resolución de vídeo.
+
+```python3 create.py --video nombre_del_video.mp4```
+
+
+* ¿Hay control de errores?
+
+He dicho rápido y sucio!
+
--- create.py
+++ create.py
@@ -16,18 +16,45 @@
 
 from moviepy.editor import concatenate_videoclips
 from moviepy.editor import VideoFileClip, ImageClip
+from pathlib import Path
 import argparse
 
+def info(nombre):
+    nombre = Path(nombre).stem
+    num = nombre.split('-')[0]
+    info = VideoFileClip('{}.mp4'.format(nombre)).subclip(0, 5)
+    print ("info: h: {}, w: {}".format(info.h, info.w))
+
+
+def generate():
+    """Genera los vídeos de finalización"""
+    for png in Path('.').glob('end_*.png'):
+        nombre = png.stem
+        res = nombre.split('_')[1]
+        clip = ImageClip(png.name).set_duration(5)
+        clip.write_videofile('{}.mp4'.format(nombre, res), fps=24)
+        print ("info: h: {}, w: {}".format(clip.h, clip.w))
+        clip.close()
+
 def convert(nombre):
+    # Obetenemos resolución
+    nombre = Path(nombre).stem
+    num = nombre.split('-')[0]
+    info = VideoFileClip('{}.mp4'.format(nombre)).subclip(0, 5)
+    res = info.h
+
     # Generamos la entradilla
-    clip = ImageClip('{}.png'.format(nombre)).set_duration(5)
-    clip.write_videofile('{}_start.mp4'.format(nombre), fps=24)
-    clip.close()
+    for png in Path('.').glob('{}-*_{}.png'.format(num, res)):
+        clip = ImageClip(png.name).set_duration(5)
+        clip.write_videofile('{}_start.mp4'.format(png.stem), fps=24)
+        clip.close()
+        entradilla = png
+
 
     # Generamos vídeo final
-    clip1 = VideoFileClip('{}_start.mp4'.format(nombre))
+    clip1 = VideoFileClip('{}_start.mp4'.format(entradilla.stem))
     clip2 = VideoFileClip('{}.mp4'.format(nombre))
-    clip3 = VideoFileClip('video_end.mp4')
+    clip3 = VideoFileClip('end_{}.mp4'.format(res))
 
     final = concatenate_videoclips([clip1, clip2, clip3], method="compose")
     final.write_videofile('{}_final.mp4'.format(nombre))
@@ -36,9 +63,15 @@ def convert(nombre):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--video', help='nombre del video principal sin mp4')
+    parser.add_argument('--info', help="muestra las dimensiones del video")
+    parser.add_argument('--generate', help="genera los finales, use --generate all")
     args = parser.parse_args()
     if args.video:
         convert(args.video)
+    elif args.info:
+        info(args.info)
+    elif args.generate:
+        generate()
     else:
         print (__doc__)
 
